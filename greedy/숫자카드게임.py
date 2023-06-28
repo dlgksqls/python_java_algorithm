e896dd4f221c96d8d9a046d0a3e7a070878ea856
LeeHB <horainlee87@gmail.com>
2023-06-28 13:34:18 +0900

    greddy

--- greedy/숫자카드게임.py
+++ greedy/숫자카드게임.py
@@ -1,16 +1,22 @@
-n,m = map(int,input().split())
+# n행 m열로 카드를 놓음
+# 뽑고자 하는 카드가 포함되어있는 행 선택
+# 선택된 행에 포함된 카드 중 가장 숫자가 낮은 카드 뽑아야함
+# 결론 = > 각 행의 카드 중 가장 작은 것 중 가장 큰 것을 골라야함
+# ex) [[3,1,2],[4,1,4],[2,2,2]]/ 출력 : 2
 
-result_list = []
-for i in range(0,n):
+n,m = map(int,input().split()) 
+
+result_list = [] # 2차원 배열을 위해 배열 하나 만듬
+for i in range(0,n): # n행 ,m열 만큼 값을 집어넣음
     line = []
     for j in range(0,m):
         number = input()
         line.append(number)
-    result_list.append(line)
+    result_list.append(line) # 큰 행렬안에 작은 행렬 넣음으로써 2차원 배열 완성 result_list[line[]]
 
-min_ = min(result_list[0])
+min_ = min(result_list[0]) # 0행에 있는 행렬 중 최솟값을 변수에 넣음
 
-for i in range(0,n):
-    if min_ < min(result_list[i]):
+for i in range(0,n): # 행의 수대로 반복문을 돌려서 각 행의 최솟값을 비교해서 최종 결론 도출
+    if min_ < min(result_list[i]): 
         min_ = min(result_list[i])
 print("카드게임 결과 : ",min_)
--- greedy/큰 수 법칙.py
+++ greedy/큰 수 법칙.py
@@ -1,22 +1,26 @@
+# n,m,k 입력받음
+# n은 받을 수 있는 숫자 갯수, m은 총 더하는 횟수, k는 같은 배열의 수를 최대로 더할 수 있는 횟수
+# n=5,m=8,k=3 [2,4,5,4,6] => 6+6+6+5+6+6+6+5 = 46
+
 n,m,k = map(int,input().split())
 
-number = []
+number = [] 
 sum = 0
 
-for i in range(0,n):
+for i in range(0,n): # n만큼 숫자 받아서 배열 생성
     num = int(input())
     number.append(num)
 
-number.sort(reverse=True)
+number.sort(reverse=True) # 내림차순으로 정렬
 
-i = 0
+i = 0 # i는 더하기 연산 횟수
 
-while i < m:
-    for j in range(0,k):
-        i = i + 1
+while i < m: # 최대 연산횟수보다 작으면 반복문
+    for j in range(0,k): # k번 가장 큰 수 덧셈 연산 시작
+        i = i + 1 # 연산 횟수 증가
         if i < m:
             sum = sum + number[0]
-    i = i + 1
+    i = i + 1 # 반복문 나와서 그 다음 큰 수 덧셈
     sum = sum + number[1]
     
 print(sum)
