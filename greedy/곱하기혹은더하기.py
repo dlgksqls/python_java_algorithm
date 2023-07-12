866d40b7d58720b57d1eb46d2afd92e3460c7eba
LeeHB <horainlee87@gmail.com>
2023-07-12 09:27:47 +0900

    python

--- greedy/곱하기혹은더하기.py
+++ greedy/곱하기혹은더하기.py
@@ -1,15 +1,18 @@
+# 02984 -> (((0+2)*9)*8)
+# 1이나 0은 곱하지말고 더하기
+
 a = input()
 
 sum = 0
 
 for i in range(len(a)):
-    key = int(a[i])
+    key = int(a[i]) # 원소 하나씩 불러오기
     
-    if (sum == 1 or sum == 0) or (key == 0 or key == 1):
+    if (sum == 1 or sum == 0) or (key == 0 or key == 1): # 첫 key가 1이나 0이면 더해주기
         sum = sum + key
     else:
-        if i == 0:
+        if i == 0: # 첫번째 원소라면 sum = key로
             sum = key
-        else:
+        else: # 아니면 곱해주기
             sum = sum*key
 print(sum)
--- greedy/모험가 길드.py
+++ greedy/모험가 길드.py
@@ -1,15 +1,15 @@
-N = int(input())
+N = int(input()) # 모험가의 수
 
-n=list(map(int,input().split()))
-n.sort(reverse=True)
+n=list(map(int,input().split())) # 각 모험가의 공포도
+n.sort(reverse=True) # 공포도가 가장 높은 순대로 정렬
 a = []
 count = 0
 
 for i in range(len(n)):
     a.append(n[i])
-    if len(a) == max(a):
-        a.clear()
-        count = count+1
+    if len(a) == max(a): # 배열의 크기가 공포도가 가장 높은 사람의 공포도와 같다면 팀 생성 종료
+        a.clear() # 배열을 비우고 이후 팀 생성
+        count = count+1 
         
 print(count)
         
