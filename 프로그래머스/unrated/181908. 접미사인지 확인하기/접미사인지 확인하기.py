dccc2b69fb7a715703ebf44075efbe2aaa0c5a17
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2023-07-20 14:10:45 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -0,0 +1,8 @@
+def solution(my_string, is_suffix):
+    answer = 0
+    for i in range(len(my_string)):
+        if is_suffix == my_string[i:]:
+            answer = answer + 1
+        else:
+            pass
+    return answer
