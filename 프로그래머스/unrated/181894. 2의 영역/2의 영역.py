5776189d01b05aee5ceb3694ed120cd9d5c37df9
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2023-07-27 16:24:05 +0900

    Update 2의 영역.py

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
@@ -1,4 +1,10 @@
 def solution(arr):
+    answer = []
+    check=[]
     if 2 not in arr:
         return [-1]
-    return arr[arr.index(2) : len(arr) - arr[::-1].index(2)]
+    else:
+        for i in range(0, len(arr)):
+            if arr[i]==2:
+                check.append(i)
+    return arr[check[0]:check[-1]+1]
