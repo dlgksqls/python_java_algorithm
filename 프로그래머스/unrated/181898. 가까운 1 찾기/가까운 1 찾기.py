ffb08a092383879ed15885d64ef8c016c5ace3cf
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2023-07-27 14:25:11 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -0,0 +1,12 @@
+def solution(arr, idx):
+    answer = 0
+    
+    for i in range(idx,len(arr)):
+        if arr[i] == 1:
+            answer = i
+            break
+        else:
+            pass
+        answer = -1
+        
+    return answer
