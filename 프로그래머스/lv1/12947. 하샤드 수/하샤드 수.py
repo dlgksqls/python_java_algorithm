b632d8b3a9fe741bf22e3c058e84f0c70acc3c1f
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2023-08-03 14:36:33 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
@@ -0,0 +1,12 @@
+def solution(x):
+    answer = True
+    sum = 0
+    y = str(x)
+    for i in range(len(y)):
+        sum += int(y[i])
+        
+    if x % sum == 0:
+        return True
+    else:
+        return False
+    
