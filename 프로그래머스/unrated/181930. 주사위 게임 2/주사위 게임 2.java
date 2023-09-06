70ef852ae07b61f7d4b0ceb5721875696e0e63aa
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2023-09-06 11:03:11 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        
+        if (a == b && b == c && c == a)
+            return (a+b+c) * (a*a+ b*b + c*c) * (a*a*a+ b*b*b + c*c*c);
+        else if(a!=b && b!=c && c!=a)
+            return a+b+c;
+        else
+            return (a+b+c)* (a*a+ b*b + c*c);
+    }
+}
