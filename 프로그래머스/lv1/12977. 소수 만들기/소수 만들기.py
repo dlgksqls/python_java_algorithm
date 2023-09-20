f6a90b86f2c585008d05c65db78f2fe5464c3a65
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2023-09-20 15:40:35 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,23 @@
+def solution(nums):
+    answer = 0
+    result = []
+    sum = 0
+    
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            for k in range(j+1,len(nums)):
+                sum = nums[i] + nums[j] + nums[k]
+                result.append(sum)
+    
+    print(result)
+    
+    for z in result:
+        count = 0
+        for x in range(2,z):
+            if z % x != 0:
+                count += 1
+        if count == z-2:
+            answer += 1
+        
+    
+    return answer
