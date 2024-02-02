9ee70ec1a47d2722140f8f28e723c2cb9949ed58
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2024-02-02 19:30:56 +0900

    [Silver I] Title: 그림, Time: 324 ms, Memory: 35060 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,4 +1,6 @@
 import sys
+from collections import deque
+
 sys.setrecursionlimit(10**9)
 count = 0
 
@@ -18,6 +20,26 @@ def dfs(x, y):
                 count += 1
                 dfs(nx, ny)
 
+def bfs(x, y):
+    global count
+    queue = deque([(x, y)])
+    graph[x][y] = 0
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    while queue:
+        v, w = queue.popleft()
+
+        for k in range(4):
+            nx = v + dx[k]
+            ny = w + dy[k]
+
+            if (0<=nx<n) and (0<=ny<m):
+                if graph[nx][ny] != 0:
+                    count += 1
+                    queue.append((nx, ny))
+                    graph[nx][ny] = 0
+
 n, m = map(int, sys.stdin.readline().split())
 
 answer = []
@@ -31,8 +53,16 @@ for i in range(n):
         count = 0
         if graph[i][j] == 1:
             count += 1
-            dfs(i, j)
+            bfs(i, j)
             answer.append(count)
+
+# for i in range(n):
+#     for j in range(m):
+#         count = 0
+#         if graph[i][j] == 1:
+#             count += 1
+#             dfs(i, j)
+#             answer.append(count)
 
 if len(answer) == 0:
     print(len(answer))
