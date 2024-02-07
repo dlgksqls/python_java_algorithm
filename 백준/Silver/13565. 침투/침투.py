69e746c0dc002cc6b13a75a557d165a0a7652137
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2024-02-07 14:08:18 +0900

    [Silver II] Title: 침투, Time: 596 ms, Memory: 38260 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.py
+++ 백준/Silver/13565. 침투/침투.py
@@ -1,34 +1,40 @@
 import sys
+from collections import deque
 
-sys.setrecursionlimit(100000000)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+answer = False
 
-# 상하좌우
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+def bfs(x, y):
+    global answer
+    queue = deque([(x, y)])
+    graph[x][y] = -1
 
+    while queue:
+        v, w = queue.popleft()        
+        
+        for i in range(4):
+            nx = dx[i] + v
+            ny = dy[i] + w
 
-def dfs(x, y):
-    visited[x][y] = True
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0 and not visited[nx][ny]:
-            dfs(nx, ny)
-
-
-n, m = map(int, sys.stdin.readline().split())
+            if (0<=nx<m) and (0<=ny<n):
+                if graph[nx][ny] == 0:
+                    queue.append((nx, ny))
+                    graph[nx][ny] = -1
+                    if nx == (m-1):
+                        answer = True
 
 graph = []
-for _ in range(n):
+m, n = map(int, sys.stdin.readline().split())
+
+for _ in range(m):
     graph.append(list(map(int, sys.stdin.readline().rstrip())))
 
-visited = [[False] * m for i in range(n)]
+for i in range(n):
+    if graph[0][i] == 0:
+        bfs(0, i)
 
-for y in range(m):
-    if graph[0][y] == 0 and not visited[0][y]:
-        dfs(0, y)
-
-if True in visited[n - 1]:
+if answer:
     print("YES")
 else:
     print("NO")
