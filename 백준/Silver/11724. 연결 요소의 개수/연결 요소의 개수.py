2ec73d56512eb006220fcab08b0ccfbd292bbec8
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2024-02-07 13:05:16 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 668 ms, Memory: 65268 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,26 +1,32 @@
 import sys
+from collections import deque
 
-sys.setrecursionlimit(10000)
+def bfs(start):
+    visited[start] = 1
+    queue = deque([start])
 
-def dfs(v):
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(i)
+    while queue:
+        v = queue.popleft()
+
+        for j in graph[v]:
+            if visited[j] == 0:
+                queue.append(j)
+                visited[j] = 1
 
 n, m = map(int, sys.stdin.readline().split())
-graph = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
-count = 0
+
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
 
 for _ in range(m):
     a, b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
 
-for i in range(1, n + 1):
-    if not visited[i]:
-        dfs(i)
-        count += 1
+count = 0
 
+for i in range(1, n+1):
+    if visited[i] == 0:
+        count += 1
+        bfs(i)
 print(count)
