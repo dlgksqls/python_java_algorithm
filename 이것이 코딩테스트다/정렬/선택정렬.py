dfdb738d0db7bf407af22367d4fd13ae50391f6a
unknown <horainlee87@gmail.com>
2024-02-13 14:47:47 +0900

    정렬 알고리즘 학습

---
+++ 이것이 코딩테스트다/정렬/선택정렬.py
@@ -0,0 +1,10 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(array)):
+    min_num = i
+    for j in range(i+1, len(array)):
+        if array[min_num] > array[j]:
+            min_num = j
+    array[i], array[min_num] = array[min_num], array[i]
+
+print(array)
