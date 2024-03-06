ec814ac0bb2b3c0bdf0e4a5f713948c2a9bf802a
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2024-03-06 20:26:21 +0700

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,19 +1,19 @@
 def solution(N, stages):
-    stage_count = [0] * (N+2)
+    answer = []
+    length = len(stages)
     
-    total_player = len(stages)  
-
-    for stage in stages: 
-        stage_count[stage] += 1
-
-    fail_rates = []  
-    for i in range(1, N+1): 
-        if total_player == 0: 
-            fail_rates.append((i, 0))
+    for i in range(1, N+1):
+        count = stages.count(i)
+        
+        if length == 0:
+            fail = 0
         else:
-            fail_rate = stage_count[i] / total_player  
-            fail_rates.append((i, fail_rate)) 
-            total_player -= stage_count[i]  
-    answer = [stage for stage, rate in sorted(fail_rates, key=lambda x: (-x[1], x[0]))]
+            fail = count / length
+            
+        answer.append((i, fail))
+        length -= count
+        
+    answer = sorted(answer, key = lambda x: x[1], reverse=True)
+    answer = [i[0] for i in answer]
     
     return answer
