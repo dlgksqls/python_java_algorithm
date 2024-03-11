77cae52e0e1c8b90e3e0fd27bad59eebddd024c9
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2024-03-11 11:53:14 +0900

    [Silver V] Title: 다리 놓기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,29 +1,25 @@
-num = int(input())
-answer_list = []
-answer = []
+# dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
 
-for i in range(num):
-  n,p = map(int,input().split())
-  answer_list.append((n,p))
+import sys
 
-for j in range(len(answer_list)):
-  max = 1
-  min = 1
-  max_min = 1
+dp = [[0 for _ in range(30)] for _ in range(30)]
 
-  if answer_list[j][0] == answer_list[j][1]:
-    answer.append(1)
-  elif answer_list[j][0] == 1:
-    answer.append(answer_list[j][1])
-  else:
-    for i in range(1, answer_list[j][1]+1):
-      max *= i
-    for i in range(1, answer_list[j][0]+1):
-      min *= i
-    for i in range(1, (answer_list[j][1]-answer_list[j][0])+1):
-      max_min *= i
-      
-    answer.append(max // (min * max_min))
+t = int(sys.stdin.readline())
+graph = []
 
-for i in answer:
-  print(i)
+for _ in range(t):
+    data = list(map(int, sys.stdin.readline().split()))
+    graph.append(data)
+
+for i in range(30):
+    for j in range(30):
+        if i == 1:
+            dp[i][j] = j
+        else:
+            if i == j:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
+
+for i, j in graph:
+    print(dp[i][j])
