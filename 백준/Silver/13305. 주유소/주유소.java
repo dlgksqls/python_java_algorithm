abbd0abed4059dbf3fd5a0a762e9a4f648ccbe10
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2024-04-12 23:05:02 +0900

    [Silver III] Title: 주유소, Time: 1160 ms, Memory: 184824 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,38 +1,38 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int n = Integer.parseInt(br.readLine());
+        // 도시의 수 N 입력받기
+        int N = sc.nextInt();
 
-        long city[] = new long[n];
-        long road[] = new long[n-1];
-
-        long answer = 0;
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i=0; i<road.length; i++){
-            road[i] = Integer.parseInt(st.nextToken());
+        // 각 도로의 길이를 저장할 배열
+        long[] roads = new long[N - 1];
+        for (int i = 0; i < N - 1; i++) {
+            roads[i] = sc.nextLong();
         }
 
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i=0; i<city.length; i++){
-            city[i] = Integer.parseInt(st.nextToken());
+        // 각 도시의 주유소 가격을 저장할 배열
+        long[] prices = new long[N];
+        for (int i = 0; i < N; i++) {
+            prices[i] = sc.nextLong();
         }
 
-        long min_road = city[0];
+        // 최소 비용 계산
+        long totalCost = 0;
+        long minPrice = prices[0]; // 첫 도시의 주유소 가격을 초기 최소 가격으로 설정
 
-        for (int i=0; i<n-1; i++){
-            if (road[i] < min_road){
-                min_road = city[i];
+        for (int i = 0; i < N - 1; i++) {
+            // 현재 도시의 주유소 가격이 이전까지의 최소 가격보다 작으면 갱신
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
             }
-            answer += (min_road * road[i]);
+            // 현재 도시에서 다음 도시까지 이동하는 데 필요한 비용을 누적 합산
+            totalCost += (minPrice * roads[i]);
         }
-        bw.write(String.valueOf(answer));
 
-        br.close();
-        bw.close();
+        System.out.println(totalCost);
+        sc.close();
     }
 }
