64b674f8281d398e72d32934c7ad74b8c9de7092
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2024-04-19 13:22:25 +0900

    [Silver II] Title: 침투, Time: 380 ms, Memory: 41196 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.java
+++ 백준/Silver/13565. 침투/침투.java
@@ -1,32 +1,40 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    static int graph [][];
+    static int graph[][];
     static int m, n;
-    static int[] dx = {0,1,0,-1};
-    static int[] dy = {1,0,-1,0};
-    static String answer = "NO";
+    static int dx[] = {-1, 1, 0, 0};
+    static int dy[] = {0, 0, -1, 1};
+    static Queue<int []> queue;
+    static boolean answer;
 
-    public static void dfs(int x, int y){
+    public static void bfs(int x, int y){
+        queue = new LinkedList<>();
+        queue.offer(new int[] {x, y});
         graph[x][y] = 1;
-        for (int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
 
-            if (nx>=0 && nx<m && ny>=0 && ny<n){
-                if (graph[nx][ny] == 0) {
-                    if (nx == m - 1) {
-                        answer = "YES";
-                        return;
+        while (!queue.isEmpty()){
+            int poll[] = queue.poll();
+
+            for (int i=0; i<4; i++){
+                int nx = poll[0] + dx[i];
+                int ny = poll[1] + dy[i];
+
+                if (nx>=0 && nx<m && ny>=0 && ny<n){
+                    if (graph[nx][ny] == 0){
+                        if (nx == m-1){
+                            answer = true;
+                            return;
+                        }
+                        queue.offer(new int[] {nx, ny});
+                        graph[nx][ny] = 1;
                     }
-                    dfs(nx, ny);
                 }
             }
         }
     }
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -35,23 +43,23 @@ public class Main {
 
         graph = new int[m][n];
 
-        for (int i=0; i<m; i++){
-            String str = br.readLine();
-            for (int j=0; j<n; j++)
-                graph[i][j] = str.charAt(j) - '0';
-        }
-
-        for (int i=0; i<n; i++){
-            if (graph[0][i] == 0){
-                dfs(0, i);
-                if (answer.equals("YES"))
-                    break;
+        for (int i=0; i<m; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = input.charAt(j) - '0';
             }
         }
 
-        bw.write(answer);
+        for (int i=0; i<n; i++){
+            if (graph[0][i] == 0)
+                bfs(0, i);
+        }
+
+        if (answer)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
 
         br.close();
-        bw.close();
     }
 }
