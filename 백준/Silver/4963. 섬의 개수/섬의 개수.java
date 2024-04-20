ca103cfca281629a60f11777427490d5429c30a0
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2024-04-20 13:50:06 +0900

    [Silver II] Title: 섬의 개수, Time: 176 ms, Memory: 16100 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,57 +1,65 @@
 import java.io.*;
 import java.util.*;
 public class Main {
+    static int graph[][];
     static int w, h;
-    static int answer;
-    static String input;
-    static Integer graph[][];
-    static int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
-    static int dy[] = {1, 1, 0, -1, -1, -1, 0, 1};
-    public static void dfs(int x, int y) {
+    static Queue<int []> queue;
+    static int dx[] = {-1, 1, 0, 0, -1, 1, 1, -1};
+    static int dy[] = {0, 0, 1, -1, 1, 1, -1, -1};
+    public static void bfs(int x, int y){
+        queue = new LinkedList<>();
+
+        queue.offer(new int [] {x, y});
         graph[x][y] = 0;
 
-        for (int i = 0; i < 8; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+        while (!queue.isEmpty()){
+            int poll[] = queue.poll();
 
-            if (nx>=0 && nx<h && ny>=0 && ny<w){
-                if (graph[nx][ny] == 1){
-                    dfs(nx, ny);
+            for (int i=0; i<8; i++){
+                int nx = dx[i] + poll[0];
+                int ny = dy[i] + poll[1];
+
+                if (nx>=0 && nx<h && ny>=0 && ny<w){
+                    if (graph[nx][ny] == 1){
+                        queue.offer(new int [] {nx, ny});
+                        graph[nx][ny] = 0;
+                    }
                 }
             }
         }
+
     }
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         while (true){
-                answer = 0;
-               StringTokenizer st = new StringTokenizer(br.readLine());
-               w = Integer.parseInt(st.nextToken());
-               h = Integer.parseInt(st.nextToken());
+            int answer = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
 
-               graph = new Integer[h][w];
+            if (w == 0 && h == 0)
+                break;
 
-               if (w == 0 && h == 0)
-                   break;
-               else{
-                   for (int i=0; i<h; i++){
-                       st = new StringTokenizer(br.readLine()); // 이 줄을 for문 안에 넣어서 각 줄마다 새로운 StringTokenizer 객체를 생성합니다.
-                       for (int j=0; j<w; j++){
-                           graph[i][j] = Integer.parseInt(st.nextToken()); // 공백으로 구분된 각 숫자를 정수로 변환하여 저장합니다.
-                       }
-                   }
-                    for (int i=0; i<h; i++) {
-                        for (int j = 0; j < w; j++) {
-                            if (graph[i][j] == 1) {
-                                answer += 1;
-                                dfs(i, j);
-                            }
-                        }
+            graph = new int[h][w];
+
+            for (int i=0; i<h; i++){
+                st = new StringTokenizer(br.readLine());
+                for (int j=0; j<w; j++){
+                    graph[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i=0; i<h; i++){
+                for (int j=0; j<w; j++){
+                    if (graph[i][j] == 1) {
+                        answer++;
+                        bfs(i, j);
                     }
-               }
-            bw.write(answer + "\n");
+                }
+            }
+            System.out.println(answer);
         }
         br.close();
         bw.close();
