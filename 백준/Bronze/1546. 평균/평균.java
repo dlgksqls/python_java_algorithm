0c541c333eedb20776692dff028da61eb9017d48
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-02-03 06:37:27 +0900

    [Bronze I] Title: 평균, Time: 212 ms, Memory: 18876 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,26 +1,28 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * 세준이는 기말고사를 망쳤다.
+ * 세준이는 점수를 조작해서 집에 가져가기로 했다. 일단 세준이는 자기 점수 중에 최댓값을 골랐다. 이 값을 M이라고 한다. 그리고 나서 모든 점수를 점수/M*100으로 고쳤다.
+ */
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] score = new int[n];
-        double answer = 0;
-        double avg = 0;
+        float answer = 0;
+        float [] arrays = new float[n];
 
-        for (int i = 0; i < n; i++) {
-            score[i] = sc.nextInt();
+        for (int i=0; i<n; i++){
+            arrays[i] = sc.nextInt();
         }
 
-        Arrays.sort(score);
+        Arrays.sort(arrays);
 
-        for (int i : score) {
-            avg += i;
+        for (float array : arrays) {
+            answer += array/arrays[n-1]*100;
         }
 
-        answer = avg * 100 / score[n-1] / n;
-        System.out.println(answer);
+        System.out.println(answer/n);
     }
 }
