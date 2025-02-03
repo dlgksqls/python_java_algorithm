272b9381abf8df4858d81248a24eceb76f633584
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-02-03 10:46:51 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1136 ms, Memory: 59104 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * 구간 합
@@ -10,27 +14,36 @@ public class Main {
     /**
      * 첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M이 주어진다. 둘째 줄에는 N개의 수가 주어진다. 수는 1,000보다 작거나 같은 자연수이다.
      * 셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
+     * 입력
+     * 5 3
+     * 5 4 3 2 1
+     * 1 3
+     * 2 4
+     * 5 5
      */
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br
+                = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arrays = new int[n+1];
-        int[] sum = new int[n+1];
+        StringTokenizer st
+                = new StringTokenizer(br.readLine()); // 5 3
 
-        for (int i=0; i<n; i++){
-            arrays[i+1] = sc.nextInt();
+        int n = Integer.parseInt(st.nextToken()); // 수의 갯수
+        int m = Integer.parseInt(st.nextToken()); // 합을 구해야하는 횟수
+
+        long[] sum = new long[n+1];
+
+        st = new StringTokenizer(br.readLine()); // 5 4 3 2 1
+
+        for (int i=1; i<=n; i++){
+            sum[i] = sum[i-1] + Integer.parseInt(st.nextToken()); 
         }
 
-        for (int i=1; i<n+1; i++){
-            sum[i] = sum[i-1] + arrays[i];
-        }
-
-        for (int i=0; i<m; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+        for (int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine()); // 1 3, 2 4, 5 5
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             System.out.println(sum[y] - sum[x-1]);
         }
     }
