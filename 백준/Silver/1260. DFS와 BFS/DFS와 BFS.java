c556739b8312f56f3da1d24837081491f6c98bb4
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-02-25 22:54:28 +0900

    [Silver II] Title: DFS와 BFS, Time: 304 ms, Memory: 21212 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,64 +1,87 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
+
 public class Main {
-    static boolean graph[][];
-    static boolean visited_dfs[];
-    static boolean visited_bfs[];
-    static ArrayList<Integer> queue;
-    static int n, m, v;
 
-    public static void dfs(int idx){
-        visited_dfs[idx] = true;
-        System.out.print(idx + " ");
-
-        for (int i=1; i<=n; i++){
-            if (!visited_dfs[i] && graph[idx][i])
-                dfs(i);
-        }
-    }
-
-    public static void bfs(){
-        queue = new ArrayList<>();
-
-        queue.add(v);
-        visited_bfs[v] = true;
-
-        while(!queue.isEmpty()){
-            int idx = queue.remove(0);
-            System.out.print(idx + " ");
-
-            for (int i=1; i<=n; i++){
-                if (!visited_bfs[i] && graph[idx][i]){
-                    queue.add(i);
-                    visited_bfs[i] = true;
-                    }
-            }
-        }
-    }
-    public static void main(String[] args) throws Exception{
+    static int n;
+    static int m;
+    static int v;
+    static ArrayList<Integer>[] graph_dfs;
+    static ArrayList<Integer>[] graph_bfs;
+    static boolean[] visited_dfs;
+    static boolean[] visited_bfs;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         v = Integer.parseInt(st.nextToken());
 
-        graph = new boolean[n+1][n+1];
+        graph_dfs = new ArrayList[n+1];
+        graph_bfs = new ArrayList[n+1];
+
         visited_dfs = new boolean[n+1];
         visited_bfs = new boolean[n+1];
 
+        for(int i=0; i<n+1; i++){
+            graph_dfs[i] = new ArrayList<Integer>();
+            graph_bfs[i] = new ArrayList<Integer>();
+        }
+
         for (int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            graph[x][y] = true;
-            graph[y][x] = true;
+            graph_dfs[a].add(b);
+            graph_dfs[b].add(a);
+
+            graph_bfs[a].add(b);
+            graph_bfs[b].add(a);
+        }
+
+        for (int i=1; i<=n; i++){
+            Collections.sort(graph_dfs[i]);
+            Collections.sort(graph_bfs[i]);
         }
 
         dfs(v);
         System.out.println();
-        bfs();
+        bfs(v);
+
+        br.close();
+    }
+
+    private static void dfs(int start) {
+        visited_dfs[start] = true; // 현재 정점을 방문한 것으로 표시
+        System.out.print(start + " "); // 현재 정점 출력
+
+        for (int i : graph_dfs[start]) {
+            if (!visited_dfs[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited_bfs[start] = true;
+
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+            System.out.print(now + " ");
+            for (int i:graph_bfs[now]){
+                if (!visited_bfs[i]){
+                    queue.add(i);
+                    visited_bfs[i] = true;
+                }
+            }
+        }
     }
 }
