74ea8a9b13a8a673e91401e84a8ec84e6dd78723
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-03-04 12:47:55 +0900

    [Gold V] Title: 회의실 배정, Time: 452 ms, Memory: 44372 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -22,25 +22,16 @@ public class Main {
             array[i][1] = end;
         }
 
-        Arrays.sort(array, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
+        Arrays.sort(array, (o1, o2) -> {
 
-                // 종료시간이 같을 경우 시작시간이 빠른순으로 정렬해야한다.
-                if(o1[1] == o2[1]) {
-                    return o1[0] - o2[0];
-                }
-
-                return o1[1] - o2[1];
+            // 종료시간이 같을 경우 시작시간이 빠른순으로 정렬해야한다.
+            if(o1[1] == o2[1]) {
+                return o1[0] - o2[0];
             }
-        });
 
-//        Arrays.sort(array, (s, e) -> {
-//            if (s[1] == e[1]){
-//                return s[0] - e[0];
-//            }
-//            return s[1] - e[1];
-//        });
+            return o1[1] - o2[1];
+        });
+        
 
         int answer = 0;
         int pre = 0;
