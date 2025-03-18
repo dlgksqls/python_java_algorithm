cf36c2dfa6ab69c3654d2d0f95a13e485d820826
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-03-18 13:16:30 +0900

    [Gold II] Title: 버블 소트, Time: 1500 ms, Memory: 81568 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.java
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -8,35 +8,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        Number[] array = new Number[n];
+        Data[] array = new Data[n];
 
-        for (int i=0; i<n; i++){
-            array[i] = new Number(i, Integer.parseInt(br.readLine()));
+        for(int i=0; i<n; i++){
+            array[i] = new Data(i, Integer.parseInt(br.readLine()));
         }
 
         Arrays.sort(array);
-        int max = 0;
 
+        int max = 0;
         for (int i=0; i<n; i++){
-            if (max < array[i].index - i) {
-                max = array[i].index - i;
-            }
+            if (max < array[i].index - i) max = array[i].index - i;
         }
-        System.out.println(max + 1);
+
+        System.out.println(max+1);
     }
 
-    static class Number implements Comparable<Number>{
+    private static class Data implements Comparable<Data>{
         int index;
         int value;
 
-        public Number(int index, int value) {
+        public Data(int index, int value) {
             this.index = index;
             this.value = value;
         }
 
         @Override
-        public int compareTo(Number o) { // value 기준 오름차순 정렬
-            return value - o.value;
+        public int compareTo(Data data) {
+            return this.value - data.value;
         }
     }
 }
