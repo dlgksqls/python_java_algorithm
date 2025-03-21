b0cb70dc888dfdb4ff94df8bae7094d3d38acfe7
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-03-21 19:06:05 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,51 +1,48 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    static boolean[][] graph;
-    static boolean[] visited;
-    static int N, M;
-    static int answer;
 
-    public static void dfs(int idx){
-        visited[idx] = true;
-        answer ++;
+    static int n;
+    static int m;
+    static ArrayList<Integer>[] graph;
+    static int[] visited;
+    static int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i=0; i<=N; i++){
-            if(visited[i] == false && graph[idx][i] == true){
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList[n+1];
+        visited = new int[n+1];
+
+        for (int i=1; i<=n; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i=0; i<m; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        dfs(1);
+
+        System.out.println(count);
+        br.close();
+    }
+
+    private static void dfs(int v) {
+        visited[v] = 1;
+
+        for (int i : graph[v]) {
+            if (visited[i] != 1){
+                count++;
                 dfs(i);
             }
         }
-    }
-
-    public static void main(String[] args) throws IOException{
-        // 1. 입력 및 초기화
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        N = Integer.parseInt(br.readLine()); // br.readLine() 한 줄 입력받음
-        M = Integer.parseInt(br.readLine());
-
-        graph = new boolean[N+1][N+1];
-        visited = new boolean[N+1];
-
-        // 2. graph에 연결 정보 채우기
-        int x, y;
-        for (int i=0; i<M; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine()); // 한 줄에 입력받은 것을 string으 뛰어쓰기 해서 나눠줌
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
-
-            graph[x][y] = true;
-            graph[y][x] = true;
-        }
-
-        // 3. dfs(재귀 함수) 호출
-        dfs(1);
-
-        // 4. 출력
-        bw.write(String.valueOf(answer - 1));
-
-        br.close();
-        bw.close();
     }
 }
