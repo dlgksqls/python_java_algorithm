deeaf67e2079c5e87b6e67bad325aa966dc128ac
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-03-24 14:13:31 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1012 ms, Memory: 114008 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,42 +1,44 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
+
+    static int n;
     static int[] array;
     static int[] temp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        array = new int[n+1];
-        temp = new int[n+1];
+        array = new int[n];
+        temp = new int[n];
 
-        for(int i=1; i<=n; i++){
+        for(int i=0; i<n; i++){
             array[i] = Integer.parseInt(br.readLine());
         }
 
-        merge_sort(1, n);
+        merge_sort(0, n-1);
 
-        for (int i=1; i<=n; i++){
+        for (int i=0; i<n; i++){
             bw.write(array[i] + "\n");
         }
+
         bw.flush();
         bw.close();
         br.close();
     }
 
     private static void merge_sort(int start, int end) {
-        if (start>=end){
+        if (start >= end){
             return;
         }
 
-        int mid = start + (end - start) / 2;
-        merge_sort(start, mid);
-        merge_sort(mid + 1, end);
+        int mid = start + (end - start)/2;
 
-        for (int i=start; i<=end; i++){
+        merge_sort(start, mid);
+        merge_sort(mid+1, end);
+
+        for(int i=start; i<=end; i++){
             temp[i] = array[i];
         }
 
@@ -44,18 +46,19 @@ public class Main {
         int index1 = start;
         int index2 = mid + 1;
 
-        while(index1 <= mid && index2 <= end){
+        while (index1 <= mid && index2 <= end){
             if (temp[index1] > temp[index2]){
                 array[k] = temp[index2];
-                k ++;
+                k++;
                 index2 ++;
             }
-            else {
+            else{
                 array[k] = temp[index1];
-                k ++;
+                k++;
                 index1 ++;
             }
         }
+
         while(index1 <= mid){
             array[k] = temp[index1];
             k ++;
