2b91aeb2a9a3881dd731c752f0be3622e6df2b32
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-03-25 20:03:58 +0900

    [Silver IV] Title: 수 찾기, Time: 1060 ms, Memory: 54428 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,10 +5,9 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int n;
-    static int[] array;
     static int m;
+    static int[] array;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,37 +16,37 @@ public class Main {
         array = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
             array[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(array);
 
         m = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            int find = Integer.parseInt(st.nextToken());
-            binary_search(find, 0, n - 1);
+        for(int i=0; i<m; i++){
+            binary_search(Integer.parseInt(st.nextToken()), 0, n-1);
         }
-
         br.close();
     }
 
-    private static void binary_search(int find, int left, int right) {
-        if (left > right) {
+    private static void binary_search(int number, int start, int end) {
+
+        if (start > end) {
             System.out.println(0);
             return;
         }
 
-        int mid = (left + right) / 2;
-
-        if (array[mid] == find) {
+        int mid = (start + end) / 2;
+        if (number == array[mid]){
             System.out.println(1);
-        } else if (array[mid] > find) {
-            binary_search(find, left, mid - 1);
-        } else {
-            binary_search(find, mid + 1, right);
+            return;
+        }
+        if (number > array[mid]){
+            binary_search(number, mid + 1, end);
+        }
+        else if (number < array[mid]){
+            binary_search(number, start, mid - 1);
         }
     }
 }
