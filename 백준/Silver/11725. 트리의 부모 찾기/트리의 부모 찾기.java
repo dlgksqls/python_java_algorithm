ccb8f971648c8b959056381c1aac1497c2c20305
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-04-17 14:07:36 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1048 ms, Memory: 79984 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,50 +1,52 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class Main {
 
     static int n;
+    static boolean[] visited;
+    static int[] parents;
     static ArrayList<Integer>[] graph;
-    static int[] visited;
-    static int[] result;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         n = Integer.parseInt(br.readLine());
+        visited = new boolean[n+1];
+        parents = new int[n+1];
         graph = new ArrayList[n+1];
-        visited = new int[n+1];
-        result = new int[n+1];
 
-        for (int i=1; i<=n; i++){
+        for(int i=1; i<=n; i++){
             graph[i] = new ArrayList<>();
         }
 
-        for (int i=0; i<n-1; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            graph[a].add(b);
-            graph[b].add(a);
+        for(int i=0; i<n-1; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x].add(y);
+            graph[y].add(x);
         }
+        dfs(1);
 
-        for(int i=1; i<=n; i++){
-            if (visited[i] != 1) {
-                dfs(i);
-            }
-        }
         for(int i=2; i<=n; i++){
-            System.out.println(result[i]);
+            System.out.println(parents[i]);
         }
+
+        br.close();
     }
 
-    private static void dfs(int v) {
-        visited[v] = 1;
+    private static void dfs(int node) {
+        visited[node] = true;
 
-        for (int i : graph[v]){
-            if (visited[i] != 1) {
-                result[i] = v;
-                dfs(i);
+        for (Integer v : graph[node]) {
+            if (!visited[v]){
+                parents[v] = node;
+                visited[v] = true;
+                dfs(v);
             }
         }
     }
