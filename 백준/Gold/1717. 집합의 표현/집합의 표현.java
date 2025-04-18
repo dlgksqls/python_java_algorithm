dfab08621362c5352268af06daf1a1c6092f48ad
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-04-18 11:56:40 +0900

    [Gold V] Title: 집합의 표현, Time: 948 ms, Memory: 60768 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -4,64 +4,50 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int n;
     static int m;
     static int[] array;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         array = new int[n+1];
 
-        for(int i=0; i<=n; i++){
+        for(int i=1; i<=n; i++){
             array[i] = i;
         }
 
         for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
-            int check = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            if (check == 0){
+            if (x == 0){
                 union(a, b);
-                continue;
             }
-            if (checkSame(a, b)) {
-                System.out.println("YES");
-                continue;
+            else if (x == 1){
+                if (find(a) == find(b)) System.out.println("YES");
+                else System.out.println("NO");
             }
-            System.out.println("NO");
         }
+
+        br.close();
     }
 
-    private static boolean checkSame(int a, int b) {
-        a = find(a);
-        b = find(b);
-
-        if (a == b){
-            return true;
+    private static int find(int a) {
+        if (array[a] == a) return a;
+        else {
+            array[a] = find(array[a]);
+            return find(array[a]);
         }
-        return false;
     }
 
     private static void union(int a, int b) {
         a = find(a);
         b = find(b);
-        if (a != b){
-            array[b] = a;
-        }
-    }
-
-    private static int find(int a) {
-        if (a == array[a]){
-            return a;
-        }
-        else {
-            return array[a] = find(array[a]);
-        }
+        array[b] = a;
     }
 }
