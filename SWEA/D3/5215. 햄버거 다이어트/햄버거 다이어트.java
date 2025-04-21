99e2ca49ea7b4eb3773593380db75b33530554c6
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-04-21 14:52:47 +0900

    [D3] Title: 햄버거 다이어트, Time: 168 ms, Memory: 39,416 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,50 +1,44 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Solution {
 
-    static int n;
-    static int l;
+    static int n, l;
     static ArrayList<Ingre> array;
-    static int answer;
+    static int[][] dp;
 
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
+        int T = sc.nextInt();
 
         for(int tc = 1; tc <= T; tc++) {
-            answer = Integer.MIN_VALUE;
             n = sc.nextInt();
             l = sc.nextInt();
 
             array = new ArrayList<>();
+            dp = new int[n + 1][l + 1];
 
-            for(int i=0; i<n; i++){
+            for(int i = 0; i < n; i++) {
                 array.add(new Ingre(sc.nextInt(), sc.nextInt()));
             }
 
-            recursion(0, 0, 0);
+            for(int i = 1; i <= n; i++) {
+                for(int j = 0; j <= l; j++) {
+                    Ingre cur = array.get(i - 1);
+                    if (cur.kal > j) {
+                        dp[i][j] = dp[i - 1][j];
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cur.kal] + cur.score);
+                    }
+                }
+            }
 
-            System.out.println("#" + tc + " " + answer);
+            System.out.println("#" + tc + " " + dp[n][l]);
         }
-    }
-
-    private static void recursion(int idx, int score, int kal) {
-
-        if (idx == n){
-            answer = Math.max(score, answer);
-            return;
-        }
-
-        if (kal + array.get(idx).kal <= l) {
-            recursion(idx + 1, score + array.get(idx).score, kal + array.get(idx).kal);
-        }
-        recursion(idx + 1, score, kal);
     }
 
     private static class Ingre {
-        int score;
-        int kal;
+        int score, kal;
 
         public Ingre(int score, int kal) {
             this.score = score;
