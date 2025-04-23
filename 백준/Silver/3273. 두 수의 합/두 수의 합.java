7718f534a7d6030965b46745ae00a8eac6cda77a
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-04-23 12:43:15 +0900

    [Silver III] Title: 두 수의 합, Time: 348 ms, Memory: 31012 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -6,33 +6,40 @@ import java.util.StringTokenizer;
 
 // 정렬 -> 양 끝에 포인터 -> 작으면 오른쪽 포인터를, 크면 왼쪽 포인터를
 public class Main {
-
     static int n;
     static int[] array;
-    static int num;
+    static int x;
     static int answer = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         n = Integer.parseInt(br.readLine());
         array = new int[n];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
             array[i] = Integer.parseInt(st.nextToken());
         }
 
-        num = Integer.parseInt(br.readLine());
+        x = Integer.parseInt(br.readLine());
 
         Arrays.sort(array);
+
         int start = 0;
-        int end = array.length - 1;
+        int end = array.length-1;
 
         while(start < end){
-            if (array[start] + array[end] < num) start ++;
-            else if (array[start] + array[end] > num) end --;
-            else if (array[start] + array[end] == num) {
+            int sum = array[start] + array[end];
+            if (sum == x) {
                 answer ++;
+                start ++;
+                end --;
+            }
+            else if (sum < x){
+                start ++;
+            }
+            else if (sum > x){
                 end --;
             }
         }
