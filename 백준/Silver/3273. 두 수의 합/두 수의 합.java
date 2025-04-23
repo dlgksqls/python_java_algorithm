7526e4cd30d6e39ee69f46834c2eb6c89fce579e
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-04-23 12:43:57 +0900

    [Silver III] Title: 두 수의 합, Time: 304 ms, Memory: 24968 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -6,26 +6,23 @@ import java.util.StringTokenizer;
 
 // 정렬 -> 양 끝에 포인터 -> 작으면 오른쪽 포인터를, 크면 왼쪽 포인터를
 public class Main {
-    static int n;
-    static int[] array;
-    static int x;
-    static int answer = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        n = Integer.parseInt(br.readLine());
-        array = new int[n];
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
 
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
             array[i] = Integer.parseInt(st.nextToken());
         }
 
-        x = Integer.parseInt(br.readLine());
+        int x = Integer.parseInt(br.readLine());
 
         Arrays.sort(array);
 
+        int answer = 0;
         int start = 0;
         int end = array.length-1;
 
