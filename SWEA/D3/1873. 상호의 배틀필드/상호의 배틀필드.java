b211aba032de889ed4dda10ee3f56dc387fb7d8a
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-04-26 21:53:46 +0900

    [D3] Title: 상호의 배틀필드, Time: 151 ms, Memory: 30,848 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -1,108 +1,94 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-    static int H, W;
-    static char[][] array;
+
+    static int h, w;
     static int nowX, nowY;
-    static int[] dx = {0, 1, 0, -1}; // L, D, R, U
+    static char[][] array;
+    static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+        int T = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc <= T; tc ++){
+            st = new StringTokenizer(br.readLine());
+            h = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+            String input = "";
+            array = new char[h][w];
 
-        for (int tc = 1; tc <= T; tc++) {
-            H = sc.nextInt();
-            W = sc.nextInt();
-            array = new char[H][W];
-
-            for (int i = 0; i < H; i++) {
-                String line = sc.next();
-                for (int j = 0; j < W; j++) {
-                    array[i][j] = line.charAt(j);
-                    if (array[i][j] == '>' || array[i][j] == 'v' || array[i][j] == '<' || array[i][j] == '^') {
+            for(int i=0; i<h; i++){
+                input = br.readLine();
+                for(int j=0; j<w; j++){
+                    array[i][j] = input.charAt(j);
+                    if (input.charAt(j) == '>' || input.charAt(j) == 'v' || input.charAt(j) == '<' || input.charAt(j) == '^'){
                         nowX = i;
                         nowY = j;
                     }
                 }
             }
+            int num = Integer.parseInt(br.readLine());
+            String order = br.readLine();
 
-            int N = sc.nextInt();
-            String order = sc.next();
-
-            // 명령어 처리
-            for (int i = 0; i < N; i++) {
-                char cmd = order.charAt(i);
-                play(cmd);
+            for(int i=0; i<num; i++){
+                switch (order.charAt(i)){
+                    case 'R' : move(0, '>'); break;
+                    case 'D' : move(1, 'v'); break;
+                    case 'L' : move(2, '<'); break;
+                    case 'U' : move(3, '^'); break;
+                    case 'S' : shoot(); break;
+                }
             }
 
-            // 결과 출력
             System.out.print("#" + tc + " ");
-            for (int i = 0; i < H; i++) {
-                for (int j = 0; j < W; j++) {
-                    System.out.print(array[i][j]);
+            for (char[] arrays : array) {
+                for (char aChar : arrays) {
+                    System.out.print(aChar);
                 }
                 System.out.println();
             }
         }
-        sc.close();
     }
 
-    private static void play(char cmd) {
-        switch (cmd) {
-            case 'R':
-                move(0, '>');
-                break;
-            case 'D':
-                move(1, 'v');
-                break;
-            case 'L':
-                move(2, '<');
-                break;
-            case 'U':
-                move(3, '^');
-                break;
-            case 'S':
-                shoot();
-                break;
-        }
-    }
+    private static void move(int idx, char dir) {
+        int nx = nowX + dx[idx];
+        int ny = nowY + dy[idx];
 
-    private static void move(int dir, char tank_dir) {
-        array[nowX][nowY] = tank_dir;
-
-        int nx = nowX + dx[dir];
-        int ny = nowY + dy[dir];
-
-        if (nx >= 0 && nx < H && ny >= 0 && ny < W && array[nx][ny] == '.') {
+        if (nx >= 0 && nx < h && ny >= 0 && ny < w && array[nx][ny] == '.'){
             array[nowX][nowY] = '.';
             nowX = nx;
             nowY = ny;
-            array[nowX][nowY] = tank_dir;
         }
+        array[nowX][nowY] = dir;
     }
-
     private static void shoot() {
         int dir = 0;
-        char tankShape = array[nowX][nowY];
+        char nowDir = array[nowX][nowY];
 
-        if (tankShape == '>') dir = 0;
-        else if (tankShape == 'v') dir = 1;
-        else if (tankShape == '<') dir = 2;
-        else if (tankShape == '^') dir = 3;
+        switch (nowDir){
+            case '>' : dir = 0; break;
+            case 'v' : dir = 1; break;
+            case '<' : dir = 2; break;
+            case '^' : dir = 3; break;
+        }
 
         int nx = nowX;
         int ny = nowY;
 
-        while (true) {
+        while(true){
             nx += dx[dir];
             ny += dy[dir];
 
-            if (nx < 0 || nx >= H || ny < 0 || ny >= W) break;
+            if (nx < 0 || nx >= h || ny < 0 || ny >= w) break;
 
             if (array[nx][ny] == '#') break;
 
-            if (array[nx][ny] == '*') {
+            if (array[nx][ny] == '*'){
                 array[nx][ny] = '.';
                 break;
             }
