aa000eda2b4fb2f8f3c5286d883d39b32844647b
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-04-29 14:58:32 +0900

    [D3] Title: 새샘이의 7-3-5 게임, Time: 437 ms, Memory: 110,820 KB -BaekjoonHub

--- SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.java
+++ SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.java
@@ -1,47 +1,34 @@
 import java.util.*;
 
 public class Solution {
-
-    static Integer[] array;
-    static TreeSet<Integer> treeSet;
-    static int answer;
-    static int choose;
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         int T;
-        T=sc.nextInt();
+        T = sc.nextInt();
 
-        for(int tc = 1; tc <= T; tc++) {
-            array = new Integer[7];
-            choose = 0;
+        for (int tc = 1; tc <= T; tc++) {
+            Integer[] array = new Integer[7];
+            TreeSet<Integer> hashSet = new TreeSet<>(Collections.reverseOrder());
 
             for(int i=0; i<7; i++){
                 array[i] = sc.nextInt();
             }
 
-            treeSet = new TreeSet<>();
             Arrays.sort(array, Collections.reverseOrder());
-
-            recursion(0, 0, 0);
-
             for(int i=0; i<5; i++){
-                answer = treeSet.pollLast();
+                for(int j=i+1; j<6; j++){
+                    for (int k=j+1; k<7; k++){
+                        hashSet.add(array[i] + array[j] + array[k]);
+                    }
+                }
             }
 
-            System.out.println("#" + tc + " " + answer);
+            int count = 1;
+            for (Integer i : hashSet) {
+                if (count == 5)
+                    System.out.println("#" + tc + " " + i);
+                count ++;
+            }
         }
-    }
-
-    private static void recursion(int idx, int depth, int sum) {
-        if (depth == 3){
-            treeSet.add(sum);
-            return;
-        }
-
-        if (idx >= 7) return;
-
-        recursion(idx + 1, depth + 1, sum + array[idx]);
-
-        recursion(idx + 1, depth, sum);
     }
 }
