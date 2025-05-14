639c1badbf1abfeaec7cee3c3da55b2006364ead
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-05-14 15:42:40 +0900

    [level 2] Title: H-Index, Time: 0.96 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] array) {
+        int answer = 0;
         Arrays.sort(array);
         int arr_size = array.length;
         
         for(int i=0; i<arr_size; i++){
-            int h = arr_size - i;
-            if (array[i] >= h){
-                return h;
+            if(array[i] >= arr_size - i){
+                answer = arr_size - i;
+                break;
             }
         }
-        return 0;
+        return answer;
     }
 }
