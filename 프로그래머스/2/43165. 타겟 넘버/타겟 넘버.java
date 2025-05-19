37751ce912a71d0e44a75ad71b143ba8f5c44190
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-05-19 12:03:22 +0900

    [level 2] Title: 타겟 넘버, Time: 4.65 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,23 @@
+class Solution {
+    static boolean[] visited;
+    static int answer = 0;
+    public int solution(int[] numbers, int target) {
+        
+        visited = new boolean[numbers.length];
+        recursion(0, 0, numbers, target);
+        return answer;
+    }
+    
+    void recursion(int idx, int sum, int[] numbers, int target){
+        if (idx == numbers.length){
+            if (sum == target){
+                answer ++;
+            }
+            return ;
+        }
+        
+        recursion(idx + 1, sum + numbers[idx], numbers, target);
+        
+        recursion(idx + 1, sum - numbers[idx], numbers, target);
+    }
+}
