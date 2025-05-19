b7e50e62a8e2d628a735efdb538b451b200e5401
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-05-19 16:12:36 +0900

    [level 2] Title: [3차] n진수 게임, Time: 52.21 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -6,14 +6,13 @@ class Solution {
         int num = 0;
         
         while(true){
-            String str = Integer.toString(num, n).toUpperCase();
+            // String str = Integer.toString(num, n);
+            String str = convert(num, n).toUpperCase();
             
             for(char c : str.toCharArray()){
-                if (j == m+1){
-                    j = 1;
-                }
-                if (j == p){
+                if (p == j){
                     answer += c;
+                    p += m;
                 }
                 if (answer.length() == t){
                     return answer;
@@ -22,5 +21,22 @@ class Solution {
             }
             num ++;
         }
+    }
+    
+    String convert(int num, int base){
+        if (num == 0) return "0";
+
+        String str = "";
+        while(num > 0){
+            int temp = num % base;
+            if (temp >= 10){
+                str = (char)(temp - 10 + 'a') + str;
+            }
+            else {
+                str = (char)(temp + '0') + str;
+            }
+            num /= base;
+        }
+        return str;
     }
 }
