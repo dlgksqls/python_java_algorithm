40bf461d2e35aa130951079ecb5de6af2e090171
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-05-20 15:30:04 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 113 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -5,36 +5,34 @@ public class Solution {
 
         Scanner sc = new Scanner(System.in);
         int T;
-        T=sc.nextInt();
+        T = sc.nextInt();
 
-        for(int tc = 1; tc <= T; tc++) {
+        for (int tc = 1; tc <= T; tc++) {
             String input = sc.next();
-            StringBuilder sb = new StringBuilder();
+            String result = repeat(input.length(), '0');
 
-            String result = repeat(input.length(), sb, 0).toString();
             int answer = 0;
 
             for(int i=0; i<input.length(); i++){
-                sb = new StringBuilder();
-                
                 if (input == result) break;
                 if (input.charAt(i) == result.charAt(i)) continue;
-                
-                if (input.charAt(i) == '1'){
-                    result = result.substring(0, i) + repeat(input.length()-i, sb, 1);
-                    answer ++;
+                if (input.charAt(i) == '0'){
+                    result = result.substring(0, i) + repeat(input.length() - i, '0');
                 }
-                else if (input.charAt(i) == '0'){
-                    result = result.substring(0, i) + repeat(input.length()-i, sb, 0);
-                    answer ++;
+                else if (input.charAt(i) == '1'){
+                    result = result.substring(0, i) + repeat(input.length() - i, '1');
                 }
+                answer ++;
             }
             System.out.println("#" + tc + " " + answer);
         }
     }
 
-    private static StringBuilder repeat(int len, StringBuilder sb, int number) {
-        for(int i = 0; i< len; i++) sb.append(number);
-        return sb;
+    private static String repeat(int length, char chr) {
+        String val = "";
+        for(int i=0; i<length; i++){
+            val += chr;
+        }
+        return val;
     }
 }
