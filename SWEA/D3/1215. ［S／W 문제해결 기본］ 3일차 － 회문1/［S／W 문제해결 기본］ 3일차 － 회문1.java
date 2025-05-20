12329084ba27bc0b288891e2dab2672042160197
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-05-20 14:59:55 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 125 ms, Memory: 27,136 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
@@ -1,34 +1,34 @@
 import java.util.*;
 
 public class Solution {
+    static int answer;
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
 
         for (int tc = 1; tc <= 10; tc++) {
             int n = sc.nextInt();
-            int answer = 0;
-            char[][] array = new char[8][8];
+            answer = 0;
 
-            for (int i = 0; i < 8; i++) {
+            char[][] array = new char[8][8];
+            for(int i=0; i<8; i++){
                 array[i] = sc.next().toCharArray();
             }
 
-            // 방향: 오른쪽, 아래
-            int[] dx = {0, 1};
-            int[] dy = {1, 0};
+            int[] dx = {0, 1}; // 오른쪽
+            int[] dy = {1, 0}; // 아래
+            int mid = n / 2;
 
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 8; j++) {
-                    for (int d = 0; d < 2; d++) {
-                        int nx = i + dx[d] * (n - 1);
+            for(int i=0; i<8; i++){
+                for(int j=0; j<8; j++){
+                    for(int d=0; d<2; d++) {
+                        int nx = i + dx[d] *(n - 1);
                         int ny = j + dy[d] * (n - 1);
 
-                        // 범위 초과 확인
                         if (nx >= 8 || ny >= 8) continue;
 
                         boolean flag = true;
 
-                        for (int k = 0; k < n / 2; k++) {
+                        for(int k = 0; k < mid; k++){
                             int sx = i + dx[d] * k;
                             int sy = j + dy[d] * k;
 
@@ -40,8 +40,7 @@ public class Solution {
                                 break;
                             }
                         }
-
-                        if (flag) answer++;
+                        if (flag) answer ++;
                     }
                 }
             }
