ce254b73abc7a80e3112a97d1161a3ae91771d78
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-05-20 13:52:20 +0900

    [D3] Title: N-Queen, Time: 85 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -14,22 +14,22 @@ public class Solution {
         for(int tc = 1; tc <= T; tc ++){
             int n = Integer.parseInt(br.readLine());
 
-            int[] array = new int[n+1]; // array[1] = 3; 1행 3열
+            int[] array = new int[n]; // array[1] = 3; 1행 3열
 
             answer = 0;
-            recursion(n, array, 1);
+            recursion(n, array, 0);
 
             System.out.println("#" + tc + " " + answer);
         }
     }
 
     private static void recursion(int n, int[] array, int row) {
-        if (row > n){
+        if (row == n){
             answer ++;
             return;
         }
 
-        for(int i=1; i<=n; i++){
+        for(int i=0; i<n; i++){
             array[row] = i;
             if (check(row, array)){
                 recursion(n, array, row + 1);
@@ -38,7 +38,7 @@ public class Solution {
     }
 
     private static boolean check(int row, int[] array) {
-        for(int i=1; i<row; i++){
+        for(int i=0; i<row; i++){
             if (array[i] == array[row] || Math.abs(array[i] - array[row]) == Math.abs(i - row)){
                 return false;
             }
