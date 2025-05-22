45ab468e0e8c6b2efad82d6aa254177bfd5ada9b
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-05-22 16:23:53 +0900

    [D3] Title: 교환학생, Time: 862 ms, Memory: 29,824 KB -BaekjoonHub

--- SWEA/D3/13038. 교환학생/교환학생.java
+++ SWEA/D3/13038. 교환학생/교환학생.java
@@ -1,39 +1,33 @@
 import java.util.*;
 
 public class Solution {
-
-    static int n;
-    static int[] array;
-
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
 
         for (int tc = 1; tc <= T; tc++) {
-            array = new int[7];
-            n = sc.nextInt();
-            for (int i = 0; i < 7; i++) {
-                array[i] = sc.nextInt();
+            int n = sc.nextInt();
+            int[] day = new int[7];
+
+            for(int i=0; i<7; i++){
+                day[i] = sc.nextInt();
             }
 
             int answer = Integer.MAX_VALUE;
 
             for(int i=0; i<7; i++){
-                int day = 0;
-                int idx = i;
                 int count = 0;
-
-                while(day != n){
-                    if (array[idx % 7] == 1){
-                        day ++;
+                int result = 0;
+                int idx = i;
+                while(count != n){
+                    if (day[idx % 7] == 1){
+                        count ++;
                     }
-                    count ++;
+                    result ++;
                     idx ++;
                 }
-
-                answer = Math.min(answer, count);
+                answer = Math.min(answer, result);
             }
-
             System.out.println("#" + tc + " " + answer);
         }
     }
