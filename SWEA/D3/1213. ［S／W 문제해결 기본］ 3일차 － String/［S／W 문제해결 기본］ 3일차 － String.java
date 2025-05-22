4f0ebc09c86bb89fb17259866c6254987102d325
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-05-22 14:43:10 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 109 ms, Memory: 28,032 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -7,30 +7,18 @@ public class Solution {
 
         for(int tc = 1; tc <= 10; tc++) {
             int n = sc.nextInt();
+
             String find = sc.next();
             String input = sc.next();
-
             int answer = 0;
-            int find_len = find.length();
-            int start = 0;
-            int end = find_len-1;
 
-            while(end <= input.length()-1){
-                boolean flag = true;
-                int count = 0;
-                for(int i=start; i<=end; i++){
-                    if (find.charAt(count) != input.charAt(i)) {
-                        flag = false;
-                        break;
-                    }
-                    count ++;
-                }
+            int start = 0;
+            int end = find.length() - 1;
+
+            while(end < input.length()){
+                if (input.substring(start, end + 1).equals(find)) answer ++;
                 start ++;
                 end ++;
-
-                if (flag) {
-                    answer++;
-                }
             }
 
             System.out.println("#" + tc + " " + answer);
