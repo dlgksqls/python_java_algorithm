9a363bdceb5303a9bc348ac249c289eb675bc211
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-05-22 17:29:36 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 113 ms, Memory: 26,880 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,25 +1,21 @@
 import java.util.Scanner;
 
 public class Solution {
-
-    static int answer;
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        for(int tc = 1; tc <= 10; tc++) {
+        for(int tc = 1; tc <= 10; tc ++){
             int n = sc.nextInt();
-            answer = 1;
-            recursion(0, sc.nextInt(), sc.nextInt());
-            System.out.println("#" + n + " " + answer);
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println("#" + tc + " " + recursion(a, b));
         }
     }
 
-    private static void recursion(int idx, int x, int y) {
-        if (idx == y) {
-            return;
-        }
-       
-        answer *= x;
-        recursion(idx + 1, x, y);
+    private static int recursion(int a, int b) {
+        if (b == 1) return a;
+        return a * recursion(a, b - 1);
     }
 }
