927d1f838f01657a276f6678ff78132e2e4f4f51
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-05-23 14:24:04 +0900

    [D3] Title: 새샘이의 7-3-5 게임, Time: 416 ms, Memory: 98,872 KB -BaekjoonHub

--- SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.java
+++ SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.java
@@ -8,27 +8,28 @@ public class Solution {
 
         for (int tc = 1; tc <= T; tc++) {
             Integer[] array = new Integer[7];
-            TreeSet<Integer> hashSet = new TreeSet<>(Collections.reverseOrder());
+            TreeSet<Integer> treeSet = new TreeSet<>(Collections.reverseOrder());
 
             for(int i=0; i<7; i++){
                 array[i] = sc.nextInt();
             }
-
+            
             Arrays.sort(array, Collections.reverseOrder());
+            
             for(int i=0; i<5; i++){
-                for(int j=i+1; j<6; j++){
-                    for (int k=j+1; k<7; k++){
-                        hashSet.add(array[i] + array[j] + array[k]);
+                for (int j=i+1; j<6; j++){
+                    for(int k=j+1; k<7; k++){
+                        treeSet.add(array[i] + array[j] + array[k]);
                     }
                 }
             }
-
-            int count = 1;
-            for (Integer i : hashSet) {
-                if (count == 5)
-                    System.out.println("#" + tc + " " + i);
-                count ++;
+            
+            int answer = 0;
+            for(int i=0; i<5; i++){
+                answer = treeSet.pollFirst();
             }
+
+            System.out.println("#" + tc + " " + answer);
         }
     }
 }
