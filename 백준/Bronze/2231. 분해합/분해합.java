902b392ea0420b9a876664d98448eccfcd01a9b4
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-06-02 11:46:30 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,25 +1,24 @@
 import java.io.*;
-import java.util.*;
+
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-
-        for (int i=1; i<n; i++){
-            int sum = 0;
-            int number = i;
-
-            while (number != 0){
-                sum += number % 10;
-                number = number / 10;
+        for(int i=0; i<=n ;i++){
+            int result = i;
+            int num = i;
+            while(num >= 10){
+                result += num % 10;
+                num /= 10;
             }
-
-            if (sum + i == n){
+            result += num;
+            if (n == result) {
                 System.out.println(i);
                 return;
             }
         }
+
         System.out.println(0);
     }
 }
