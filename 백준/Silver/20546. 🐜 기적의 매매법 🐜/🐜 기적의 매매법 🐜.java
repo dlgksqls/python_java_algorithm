13c58d569d0cbfa204a51ba60128f57c7aef4bf4
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-06-09 13:37:52 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 104 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
@@ -1,69 +1,55 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int money;
-    static int count_seong;
-    static int count_jun;
-    static int[] array = new int[14];
+    static int jun;
+    static int jun_count = 0;
+    static int seong;
+    static int seong_count = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        money = Integer.parseInt(br.readLine());
+        jun = seong = Integer.parseInt(br.readLine());
+        int last = 0;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i=0; i<14; i++){
-            array[i] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int down = 0;
+        int up = 0;
+        int prev = 0;
+        for(int i=0; i<14; i++){
+            int now = Integer.parseInt(st.nextToken());
+            if (jun >= now) {
+                jun_count += jun / now;
+                jun -= jun_count * now;
+            }
+
+            if (now > prev) {
+                down = 0;
+                up ++;
+            }
+            else if (now < prev) {
+                up = 0;
+                down ++;
+            }
+
+            if (up == 3){
+                seong_count += seong / now;
+                seong -= seong_count * now;
+            }
+            else if (down == 3){
+                seong += seong_count * now;
+                seong_count = 0;
+            }
+
+            last = now;
         }
 
-        int jun_money = money;
-        int seong_money = money;
-
-        jun(jun_money, 0);
-        seong(seong_money, 0);
-
-        if (count_jun > count_seong) System.out.println("BNP");
-        else if (count_jun < count_seong) System.out.println("TIMING");
+        if (jun + jun_count * last > seong + seong_count * last) System.out.println("BNP");
+        else if (jun + jun_count * last < seong + seong_count * last) System.out.println("TIMING");
         else System.out.println("SAMESAME");
-    }
-
-    private static void jun(int jun_money, int count) {
-        for (int i=0; i<14; i++){
-            if (jun_money >= array[i]) {
-                count += jun_money / array[i];
-                jun_money -= array[i] * (jun_money / array[i]);
-            }
-        }
-        count_jun = jun_money + (array[13] * count);
-    }
-
-    private static void seong(int seong_money, int count) {
-        int count_high = 0;
-        int count_low = 0;
-        int recent = array[0];
-
-        for (int i=1; i<14; i++){
-            if (recent > array[i]){
-                count_high = 0;
-                count_low ++;
-            }
-            else if (recent < array[i]){
-                count_high ++;
-                count_low = 0;
-            }
-
-            if (count_low >= 3){
-                count += seong_money / array[i];
-                seong_money -= array[i] * (seong_money / array[i]);
-            }
-            else if(count_high >= 3){
-                seong_money += count * array[i];
-                count = 0;
-            }
-            recent = array[i];
-        }
-
-        count_seong = seong_money + (array[13] * count);
+        br.close();
     }
 }
