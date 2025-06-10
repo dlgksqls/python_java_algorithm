1bf602b581fd07e6787925231c59ad04cb76ae47
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-06-10 15:56:11 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 612 ms, Memory: 18892 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -5,39 +5,43 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] array;
     static boolean[] op_visited;
+    static ArrayList<Character> op;
     static ArrayList<Character> op_count;
     static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[] array = new int[n];
+        array = new int[n];
+
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             array[i] = Integer.parseInt(st.nextToken());
         }
 
-        ArrayList<Character> op = new ArrayList<>();
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<4; i++){
-            char opChar = 0;
+        op = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            char opChar;
             if (i == 0) opChar = '+';
             else if (i == 1) opChar = '-';
             else if (i == 2) opChar = '*';
             else opChar = '/';
 
             int count = Integer.parseInt(st.nextToken());
-            for(int j=0; j<count; j++){
+            for (int j = 0; j < count; j++) {
                 op.add(opChar);
             }
         }
 
+        op_visited = new boolean[n - 1];
         op_count = new ArrayList<>();
-        op_visited = new boolean[n-1];
-        recursion(array, op);
+        recursion();
 
         System.out.println(max);
         System.out.println(min);
@@ -45,49 +49,46 @@ public class Main {
         br.close();
     }
 
-    private static void recursion(int[] array, ArrayList<Character> op) {
-
-        if (op_count.size() == op.size()){
-            int result = operator(array, op_count);
+    private static void recursion() {
+        if (op_count.size() == op.size()) {
+            int result = operator(op_count);
 
             max = Math.max(max, result);
             min = Math.min(min, result);
-
             return;
         }
 
-        for(int i=0; i<op.size(); i++){
-            if (!op_visited[i]){
+        for (int i = 0; i < op.size(); i++) {
+            if (!op_visited[i]) {
                 op_visited[i] = true;
                 op_count.add(op.get(i));
-                recursion(array, op);
-                op_count.remove(op_count.size()-1);
+                recursion();
                 op_visited[i] = false;
+                op_count.remove(op_count.size() - 1);
             }
         }
     }
 
-    private static int operator(int[] array, ArrayList<Character> op) {
+    private static int operator(ArrayList<Character> opCount) {
         int result = array[0];
-        for(int i=0; i<op.size(); i++){
-            int num1 = array[i+1];
-            char opChar = op.get(i);
 
-            if (opChar == '+') result += num1;
-            else if(opChar == '-') result -= num1;
-            else if(opChar == '*') result *= num1;
-            else if(opChar == '/'){
+        for (int i = 0; i < opCount.size(); i++) {
+            int num = array[i + 1];
+            char opChar = opCount.get(i);
+
+            if (opChar == '+') result += num;
+            else if (opChar == '-') result -= num;
+            else if (opChar == '*') result *= num;
+            else if (opChar == '/') {
                 if (result < 0) {
                     result *= -1;
-                    result /= num1;
+                    result /= num;
                     result *= -1;
-                }
-                else{
-                    result /= num1;
+                } else {
+                    result /= num;
                 }
             }
         }
-
         return result;
     }
 }
