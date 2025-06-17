dc0fb2662a7c1ffba3de12c2841a5b344825f486
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-06-17 12:24:58 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -13,15 +13,11 @@ public class Main {
         dp[0] = 0;
         dp[1] = 1;
 
-        System.out.println(recursion(n));
-        br.close();
-    }
-
-    private static long recursion(int v) {
-        if (v <= 1) return dp[v];
-        if (dp[v] == 0) {
-            dp[v] = recursion(v - 1) + recursion(v - 2);
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return dp[v];
+
+        System.out.println(dp[n]);
+        br.close();
     }
 }
