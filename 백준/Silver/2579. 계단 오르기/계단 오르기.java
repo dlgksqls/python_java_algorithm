18e7aec405b0ae5d6959cf21728f6f0c80921b28
dlgksqls <129052544+dlgksqls@users.noreply.github.com>
2025-06-21 22:12:02 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -5,40 +5,56 @@ import java.io.InputStreamReader;
 public class Main {
     static int[] array;
     static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         array = new int[n+1];
         dp = new int[n+1];
-
         for(int i=1; i<=n; i++){
             array[i] = Integer.parseInt(br.readLine());
-            dp[i] = -1;
         }
 
-        dp[0] = 0;
         if (n == 1) {
             System.out.println(array[1]);
             return;
         }
-        else if (n == 2){
+        else if (n == 2) {
             System.out.println(array[1] + array[2]);
             return;
         }
-        dp[1] = array[1];
-        dp[2] = array[1] + array[2];
 
+        initializeDp(n);
+        for(int i=3; i<=n; i++){
+            dp[i] = Math.max(dp[i - 3] + array[i - 1], dp[i - 2]) + array[i];
+        }
+//        System.out.println(dp[n]);
+
+        initializeDp(n);
         System.out.println(recursion(n));
         br.close();
     }
 
-    private static int recursion(int i) {
-        if (dp[i] != -1) return dp[i];
-        if (i == 1) return dp[1];
-        else if (i == 2) return dp[2];
+    private static int recursion(int n) {
+        if (dp[n] != -1) return dp[n];
+        if (n == 1) return dp[1];
+        if (n == 2) return dp[2];
 
-        dp[i] = Math.max(recursion(i-3) + array[i-1], recursion(i-2)) + array[i];
-        return dp[i];
+        if (n > 0) {
+            dp[n] = Math.max(recursion(n - 3) + array[n - 1], recursion(n - 2)) + array[n];
+            return dp[n];
+        }
+        return 0;
+    }
+
+    private static void initializeDp(int n){
+        dp = new int[n+1];
+        for(int i=0; i<=n; i++){
+            dp[i] = -1;
+        }
+        
+        dp[1] = array[1];
+        dp[2] = array[1] + array[2];
     }
 }
